3751163478bb4c885a9d38ced1bb0badb9511bc9
ThheKNG <kkarundu8@gmail.com>
2023-09-29 11:24:20 +0000

    Finishing touches

--- Exercise3.js
+++ Exercise3.js
@@ -2,20 +2,18 @@ let my_arr = ["data", "representation", "and","Query"];
 
 let add_task = (task) => {
     my_arr.push(task);
-    console.log(task + " added to the array");
+    console.log("Task: "+ task + " added to the array");
     console.log("Number of tasks: " + my_arr.length);
 }
 let list_all_tasks = () => {
-    for(let i = 0; i < my_arr.length; i++) {
-        console.log(i+1 + ". " + my_arr[i]);
-    }
+    my_arr.forEach((task) => {
+        console.log(task);
+    });
 }
+
 let delete_task = (task) => {
-    for(let i = 0; i < my_arr.length; i++) {
-        if(task === my_arr[i]) {
-            my_arr.splice(i, 1);
-        }
-    }
+    let index = my_arr.indexOf(task);
+    my_arr.splice(index, 1);
     console.log("Number of tasks: " + my_arr.length);
 }
 
@@ -23,5 +21,3 @@ add_task("Graphics");
 list_all_tasks();
 delete_task("and");
 list_all_tasks();
-
-
